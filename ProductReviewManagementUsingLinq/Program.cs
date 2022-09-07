11036fcad0543c308beb21af4facc11254009e20
aayush-aryan <log2aayusharyan@gmail.com>
2022-09-07 23:35:10 +0530

    added. welcome message for Product Review Management

---
+++ ProductReviewManagementUsingLinq/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ProductReviewManagementUsingLinq
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to product review Management!");
+        }
+    }
+}
